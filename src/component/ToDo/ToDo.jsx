bf92d00409f0fd591e890c2bf25446c4a2d5d4e7
razogey <razogey88@gmail.com>
2026-09-21 02:48:14 +0300

    feat: enhance task management with status toggle and improved rendering

--- src/component/ToDo/ToDo.jsx
+++ src/component/ToDo/ToDo.jsx
@@ -7,12 +7,21 @@ export const ToDo = () => {
 
     const addTask = () => {
         if (inputValue.trim() === "") return
-        setTasks([...tasks, inputValue])
+        setTasks([...tasks, {text:inputValue, completed: false}])
         setInputValue("")
     }
 
     const removeTask = (task_id) => {
-        setTasks(tasks.filter((x,i)=>i!==task_id))
+        setTasks(tasks.filter((_,i)=>i!==task_id))
+    }
+
+    const changeStatus = (targetedIndex) => {
+        setTasks(tasks?.map((task, index) => {
+            if (index === targetedIndex) {
+                return {...task, completed: !task.completed}
+            }
+            return task
+        }))
     }
 
     return (
@@ -28,8 +37,29 @@ export const ToDo = () => {
             <br />
             <button onClick={addTask}>Add Task</button>
             <ul className="list">
-                {tasks.map((task, index) => {
-                    return <li key={index} className="list-item">{task}  <button onClick={() => removeTask(index)}>Remove</button></li>
+                {tasks?.map((task, index) => {
+                    return (
+                        <li key={index} className="list-item">
+                            <span 
+                                onClick={() => changeStatus(index)}
+                                style={{ 
+                                        textDecoration: task.completed ? "line-through" : "none",
+                                        cursor: "pointer",
+                                        opacity: task.completed ? 0.6 : 1 ,
+                                        color: task.completed ? "#ff0000" : "#6b6375"
+                                    }}
+                            >
+                                {task.text}  
+                            </span>
+                            <input 
+                                type="checkbox" 
+                                name="Done"  
+                                checked={task.completed} 
+                                onChange={() => changeStatus(index)}
+                            />
+                            <button onClick={() => removeTask(index)}>Remove</button>
+                        </li>
+                    )
                 })}
             </ul>
         </div>
