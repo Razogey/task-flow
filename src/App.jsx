50a2a16269cf54b3a458c9b5c953652ddc6c052d
razogey <razogey88@gmail.com>
2026-09-20 15:02:25 +0300

    feat: initialize todo app with React and Vite
    - Create main application component (App.jsx) and ToDo component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import ToDo from "./component/ToDo/ToDo"
+
+function App() {
+  
+  return (
+    <>
+      <ToDo />
+    </>
+  )
+}
+
+export default App
